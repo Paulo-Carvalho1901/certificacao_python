2d03569eb7044e9cf74e6b2a1b2b61c56f604e3e
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-09 12:15:09 -0300

    inicio dos estudos cerficação

---
+++ 3_variables.py
@@ -0,0 +1,24 @@
+saudacao = 'Hello, friend!'
+print(saudacao)
+saudacao = 'hi, everybody'
+print(saudacao)
+
+"""
+nomes de variavel correta
+_caracteres
+nomes
+idacde
+
+nomes de variavel incorretas
+12_nomes
+nome completo
+
+variavel tambem difere 
+maiusculas de minusculas portanto
+nomes como:
+
+Nome = 'Andreia'
+nome = 'Paulo'
+são variavel diferentes
+
+"""
---
+++ 6_operatiors.py
@@ -0,0 +1,25 @@
+2 + 3
+5 - 2
+3 * 2
+
+# divisao
+6 / 2
+7 / 2
+
+# divisao inteira sempre retorna resltados inteiros
+6 // 2
+7 ** 3
+
+# modulo resto da divisao retorna resto
+6 % 2
+7 % 3
+
+# operadores de pontencia
+6 ** 3
+7 ** 2
+
+income = 250_000
+lowtaxland_rate = 0.05
+ripoffland_rate = 0.43
+ 
+print('Your income is', income, 'and you would pay', income * lowtaxland_rate, 'income tax in Lowtaxland or', income * ripoffland_rate, 'income tax in Ripoffland. You would save', income * ripoffland_rate - income * lowtaxland_rate, 'by paying taxes in Lowtaxland!')
