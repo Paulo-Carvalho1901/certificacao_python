6668691e20ff548b981bec1d40df0144734389b5
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-12 14:02:56 -0300

    bloco de if esel aninhados

---
+++ 18_Nested_if_statements.py
@@ -0,0 +1,10 @@
+answer = input('Do you like treveling? y/n: ')
+if answer == 'y':
+    answer_b = input('And do you like Asia? y/n: ')
+    if answer_b == 'y':
+        print('Excelent! You can win a ticket to Thailand! ')
+    else:
+        print('Sorry to hear that!')
+else:
+    print('Sorry to hear that!')
+
