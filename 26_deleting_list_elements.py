caa99d8481c024664c9ef13f1e0ab4f82244f441
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-20 17:16:34 -0300

    deletando itens da lista

---
+++ 26_deleting_list_elements.py
@@ -0,0 +1,6 @@
+top_cities = ['New York', 'Los Angeles', 'Singapore', 'Chicago', 'Houton', 'Phoenix']
+# del top_cities[2]
+# print(top_cities[2])
+# del top_cities[3:]
+# print(top_cities)
+# del top_cities[:]
