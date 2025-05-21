9e78d6d606d394f1f068fe5dc730f91564dc37af
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-21 17:06:02 -0300

    iteterando com as listas e mostrando seus index na lista

--- 28_iterating_list.py
+++ 28_iterating_list.py
@@ -1,6 +1,11 @@
+# top_cities = ['New York', 'Los Angeles', 'Chicago', 'Houton', 'Phoenix']
+
+# # Iterando com a lista item por item
+# for city in top_cities:
+#     print('Current city', city)
+
 top_cities = ['New York', 'Los Angeles', 'Chicago', 'Houton', 'Phoenix']
 
 # Iterando com a lista item por item
-for city in top_cities:
-    print('Current city', city)
-    
+for city_index in range(len(top_cities)):
+    print('Current index', city_index, '| Current city', top_cities[city_index], )
