e4f3e505a7cb8dc8acd6da0de773fc08e4051dde
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-21 15:35:21 -0300

    praticando com if,

--- Praticando/praticar_if.py
+++ Praticando/praticar_if.py
@@ -13,3 +13,11 @@
 #     print('É Paulo')
 # print('Está fora do if')
 
+# variavel1 = ''
+variavel1 = 'Paulo'
+variavel2 = 36
+lista = ['Paulo', 'Andreia', 'Davi', 'Carvalho']
+
+if variavel1 == 'Paulo': 
+    print('É igual')
+
