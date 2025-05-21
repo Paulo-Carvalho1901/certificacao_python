8630413b7b43e31f0f21f9905d89f23f74e8be3e
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-21 15:50:19 -0300

    Add itens na lista

---
+++ 27_add_lista_elements.py
@@ -0,0 +1,8 @@
+
+
+book_rating = [7, 9, 5, 6, 8]
+book_rating.append(4) # add um item ao final da lista
+print(book_rating)
+
+book_rating.insert(0, 55) # já o metodo insert insere um item em qualquer parte da lista
+print(book_rating)
