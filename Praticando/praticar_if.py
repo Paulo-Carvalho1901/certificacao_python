7acc4a6c7ff87a495cf5e5c34da995b358fca479
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-05-21 15:30:42 -0300

    praticando com if, estrutura de controle

---
+++ Praticando/praticar_if.py
@@ -0,0 +1,15 @@
+# Praticando estrutura de condição
+
+# idade  = 17
+
+# # Varificando se é maior de idade
+# if idade >= 18:
+#     print('Maior de idade')
+
+
+# nome = 'Carvalho'
+
+# if nome == 'Paulo':
+#     print('É Paulo')
+# print('Está fora do if')
+
