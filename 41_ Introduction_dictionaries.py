5858d523bd06fa240d49046042d5d7e080542b4b
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-06-01 02:51:13 -0300

    introdução a dicionarios

---
+++ 41_ Introduction_dictionaries.py
@@ -0,0 +1,17 @@
+emails = {
+    'Paulo Carvalho': 'paulo@hotmail.com',
+    'Andreia Cristina': 'andreia@hotmail.com',
+    'Davi': 'davi@hotmail.com'
+}
+
+print(emails['Paulo Carvalho'])
+
+
+spanish_animals = {
+    'dog': 'el perro',
+    'cat': 'el gato',
+    'horse': 'el cabello',
+    'bird': 'el pajaro'
+}
+
+print(spanish_animals['bird'])
