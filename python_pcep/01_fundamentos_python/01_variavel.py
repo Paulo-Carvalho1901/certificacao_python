51fe456f1798ceaa88d34f8619f1e8e8b11ebbb0
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-08 17:20:11 -0300

    Introdução de variaveis e suas principais sintax e semantica

---
+++ python_pcep/01_fundamentos_python/01_variavel.py
@@ -0,0 +1,44 @@
+# Introduçao ao uso de variaveis
+
+# Uma variavel é uma espaço em memoria
+# que usamos para guardar algum tipo de informaçao/dado
+
+
+# Atribuindo uma variavel
+a = 1
+b = 2
+print(a)
+print(b)
+
+print(a + b)
+
+# variavel tipo texto
+
+texto = 'todo texto é uma string'
+texto2 = 'todo texto é uma string'
+print(texto)
+print(texto2)
+
+# varificando tipo de variavel
+print(a, type(a))
+print(texto, type(texto))
+
+
+# variavel tipo float
+decimal = 1.0923
+print('Valor decimal', decimal)
+print(type(decimal))
+
+
+# Nomenclatura das variaveis
+"""
+Ma pratica
+1a
+1 a 
+@a 
+
+Boas praticas
+a1
+a_
+_a
+"""
