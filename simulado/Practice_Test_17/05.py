b3c2faba66bab320e2a32b74ffaddadfdd55f3ff
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-30 10:24:06 -0300

    finalizo exercicio escopo global

---
+++ simulado/Practice_Test_17/05.py
@@ -0,0 +1,13 @@
+# What will be the output after the following code is executed ?
+
+x = 20
+
+def func():
+    global x
+    x += 10
+    print(10 + x)
+
+
+print(x) # 20
+func() # 40
+print(x) # 30
