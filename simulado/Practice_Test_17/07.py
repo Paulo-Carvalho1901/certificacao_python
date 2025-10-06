8ac70c2a20554d008c43b246d1e631af560d132d
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-06 09:49:19 -0300

    solicitando um nome do usuario em python

---
+++ simulado/Practice_Test_17/07.py
@@ -0,0 +1,5 @@
+# Can you arrange the provided code boxes to construct a line of code that 
+# prompts the user for a string input and assigns it to the variable 
+# "Username"? Please note that not all code boxes may be used.
+
+nome_usuario = input('Digite seu nome: ')
