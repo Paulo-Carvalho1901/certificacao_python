9ea9251b51a132bb12eb71c7bbe4e0e70928827e
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-06 09:46:30 -0300

    exercicio de operaçoes precedentes

---
+++ simulado/Practice_Test_17/06.py
@@ -0,0 +1,25 @@
+# What will be the result of the following expression ?
+
+"""
+1. Exponenciação: ** (Calcula potências)
+2. Multiplicação, Divisão (ponto flutuante), Divisão Inteira e Módulo: *, /, //, % (Da esquerda para a direita)
+3. Adição e Subtração: +, - (Da esquerda para a direita)
+
+OBS: 
+Parênteses: Se você usar parênteses ( ), as operações dentro dos parênteses serão sempre executadas primeiro, 
+independentemente da precedência dos outros operadores.
+
+"""
+
+print(2 // 3 ** 2) # 0
+print(2 // 3) # 0
+print(10 + 5 * 2 - 3) # 17
+print(20 / 4 + 2 ** 3) # 13.0
+print(15 - 3 * 2 // 4) # 14
+print(7 * (10 - 5) / 2) # 17.5
+print(12 // 5 * 3 + 1) # 7
+print(2 ** 3 - 4 / 2 + 1) # 7.0
+print(9 + 16 ** 0.5 * 2) # 17.0
+print((20 - 10) / (2 + 3)) # 2
+print(3 * 2 ** 2 + 10 // 3) # 15
+print(1 + 2 * 3 - 4 / 2 ** 2) # 6.0
