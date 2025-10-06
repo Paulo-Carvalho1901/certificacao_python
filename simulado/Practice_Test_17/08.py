5f5918da8cd5da21f723ff2fa80f8b2aa4492968
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-06 09:51:51 -0300

    verificando a saida da lista com fatiamento

---
+++ simulado/Practice_Test_17/08.py
@@ -0,0 +1,5 @@
+# Assuming the following code has been successfully executed:
+
+List1 = [10,20,30,40,50]
+print(List1[2:6]) # nesse trecho retorna 30, 40, 50
+print(List1[:]) # aqui terna a lista toda
