f733dc084d6cfc3ee7a01021ed13e2a499ab992f
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-07 14:39:59 -0300

    lista tem o mesmo tamanho

---
+++ simulado/Practice_Test_18/ex16.py
@@ -0,0 +1,12 @@
+# Take a look at the snippet and choose one of the 
+# following statements which is true
+
+nums = []
+vals = nums
+vals.append(1)
+
+print(nums)
+print(vals)
+
+print(id(nums))
+print(id(vals))
