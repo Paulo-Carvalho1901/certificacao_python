c630312f25268ffc62070336da7b00b3731c61ea
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-07 09:51:02 -0300

    divisao por 2 numeros x e y

---
+++ simulado/Practice_Test_18/ex04.py
@@ -0,0 +1,10 @@
+# What is the output of the following snippet
+# if the user enters two lines containing 2 and 4 
+# respectively?
+
+x = int(input()) # 2
+y = int(input()) # 4
+
+x = x / y
+y = y / x
+print(y)
