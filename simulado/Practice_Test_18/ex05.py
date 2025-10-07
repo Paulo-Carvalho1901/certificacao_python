ae616cd4cf24273582f889288930717ee3864bc0
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-07 10:37:26 -0300

    precedenacia de operadores

---
+++ simulado/Practice_Test_18/ex05.py
@@ -0,0 +1,24 @@
+# Ordem de precedencia
+'''
+1. ()
+2. **
+3. *, /, //, %
+4. +, -
+'''
+
+
+# what is the output of the following snippet
+
+x = 1 / 2 + 3 // 3 + 4 ** 2
+
+print(x)
+
+y = 1 + 1 * 2 
+
+print(y)
+
+print(5 + 3 * 2)
+print(3 * 5 + 4 ** 2)
+print(3 * (5 + 4) ** 2)
+print(1 + 2 ** 3)
+print(1 + 4 / 2 * 3 -1)
