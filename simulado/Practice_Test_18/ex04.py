6fd047cbaec66f947e65ccf192a0ccb84a22c5c6
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-08 10:29:31 -0300

    divisao com fracioada alterando as posições dos divisores

--- simulado/Practice_Test_18/ex04.py
+++ simulado/Practice_Test_18/ex04.py
@@ -2,15 +2,15 @@
 # if the user enters two lines containing 2 and 4 
 # respectively?
 
-# x = int(input()) # 2
-# y = int(input()) # 4
+x = int(input()) # 2
+y = int(input()) # 4
 
-# x = x / y
-# y = y / x
-# print(y)
+x = x / y
+y = y / x
+print(y)
 
-print(1 / 2)
-print(2 / 4)
-print(3 / 6)
+# print(1 / 2)
+# print(2 / 4)
+# print(3 / 6)
 
-print(4 / 0.5)
+# print(4 / 0.5)
