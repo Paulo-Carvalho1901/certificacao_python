cb94125b55508a8ae7159e09bb157ce3ffe9beae
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-09 16:51:25 -0300

    fazendo a soma dos numeros da lista na variavel vals

---
+++ simulado/Practice_Test_19/ex16.py
@@ -0,0 +1,7 @@
+# After executed of the following snippet
+# the sum of all vals elements will be equal to:
+
+vals = [0, 1, 2]
+vals.insert(0, 1) # [1, 0, 1, 2]
+del vals[1] # [1, 1, 2]
+print(vals)
