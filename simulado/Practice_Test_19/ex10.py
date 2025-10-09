aca82e5c70396822a82b10acf3d6b501c6d5d1e4
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-09 02:19:16 -0300

    lista recebe lista e faz um fatiamento

---
+++ simulado/Practice_Test_19/ex10.py
@@ -0,0 +1,11 @@
+# Take a look at the snippet, and choose the true statement: (Select two answers)
+
+nums = [1, 2, 3]
+vals = nums
+del vals[1:2]
+
+print(nums)
+print(vals)
+
+print(id(nums))
+print(id(vals))
