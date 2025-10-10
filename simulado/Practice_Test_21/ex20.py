b7118b007289448fd6be5b70954b2688bc5f6069
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-10 14:11:16 -0300

    funcao de recursividade

---
+++ simulado/Practice_Test_21/ex20.py
@@ -0,0 +1,8 @@
+# What will be the output of the following code?
+
+def myfun(x):
+        if x == 0:
+                return 1
+        return x + myfun(x-1)
+  
+print(myfun(5)) # 5 + 4 + 3 + 2 + 1
