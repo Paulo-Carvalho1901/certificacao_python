27f6f426472923663231b9575190a3c3d1e804d4
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-10 13:49:27 -0300

    explicando a iteração com while e comparando valores de a

---
+++ simulado/Practice_Test_21/ex16.py
@@ -0,0 +1,12 @@
+# How many at signs (@) will be printed after the following code snippet?
+
+a = 0
+while a <= 5:
+        a += 2
+        print('@')
+
+# aqui sera iterado a com a condicional de a <= 5:
+# na primeira a é igual 0 ele incrementa 2 e print a caracter @
+# agora a vale 2 condicional a é menor ou igual a que 5 incrementa 2 e print @
+# agora a vale 4 condcional a é menor ou igual 5 incrementa 2 e print @
+# agora a vale 6 condicional não é valida para a iteração
