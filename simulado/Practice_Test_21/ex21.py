82a11c2139405e2be944f2f7973c72a9d1ee03f2
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-10 14:11:55 -0300

    condiconal if verificando valor de 0

---
+++ simulado/Practice_Test_21/ex21.py
@@ -0,0 +1,13 @@
+# what will be the output when the user provides the number 0?
+
+num = int(input('give a number: '))
+if num > 0:
+    if num < 5:
+        print('1')
+    else:
+        print('2')
+else:
+        if num < -1:
+                print('3')
+        else:
+                print('4')
