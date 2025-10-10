0bf67e9e18b0160d862ef363c1387a7771850208
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-10 13:44:48 -0300

    comparando numeros da lista com fatiamento

---
+++ simulado/Practice_Test_21/ex15.py
@@ -0,0 +1,7 @@
+# Which of the following statement(s) is/are true?
+
+list1 = [1, 2, 3]
+list2 = list1[-3:-1] # essa lista o menos -3 é o 1 e -1 o ultimo item da lista
+
+print(list1) 
+print(list2)
