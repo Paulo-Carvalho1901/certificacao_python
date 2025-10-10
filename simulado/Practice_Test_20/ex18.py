5a18aa3e6c02249f18d45d34ca27fdae9b921d31
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-10 10:06:36 -0300

    tipos de lista com fatiamento atribuindo a variavel com uma lista modificando ou nao modificando sua estrutura

--- simulado/Practice_Test_20/ex18.py
+++ simulado/Practice_Test_20/ex18.py
@@ -1,5 +1,14 @@
+
 list1 = [1, 2, 3]
 list2 = list1
 list2.append(4)
-
 print(list1)
+print(list2)
+
+print()
+
+lista_a = [1, 2, 3]
+lista_b = lista_a[:]
+lista_b.append(4)
+print(lista_a)
+print(lista_b)
