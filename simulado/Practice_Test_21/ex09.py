23faa17be2e47d9f6e773c36364e046be006f788
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-10 13:24:23 -0300

    valor de x é None ou seja um nao valor

---
+++ simulado/Practice_Test_21/ex09.py
@@ -0,0 +1,11 @@
+# what will be the output of the following code snippet?
+
+def func(x):
+        if x:
+                print('1')
+        elif x is False:
+                print('2')
+        else:
+                print('3')
+ 
+func(None)
