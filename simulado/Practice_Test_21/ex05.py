ae1cffa66f7b64fe76f2c572ee4c9ad404d83f17
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-10 13:13:42 -0300

    verificando a saindo da função das variaveis com seu escopo da função com variaveis externas

---
+++ simulado/Practice_Test_21/ex05.py
@@ -0,0 +1,10 @@
+# What will be the output of the following snippet?
+
+def show_truth():
+    mysterious_var = ['New Surprise!']
+    print(mysterious_var)
+ 
+mysterious_var = ['Surprise!']
+print(mysterious_var)
+show_truth()
+print(mysterious_var)
