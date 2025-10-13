e66ea44d64dd5980272fb2993ba401d9e660bb46
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-13 12:22:41 -0300

    estudo de exames para provar pcep python

---
+++ simulado/Practice_Test_23/ex22.py
@@ -0,0 +1,7 @@
+# What will be the output of the following code ?
+
+fruits = ["Apple","Orange","Mango"]
+for fruit in fruits:
+    if fruit != "Apple":
+
+        print(fruit,end="")
---
+++ simulado/Practice_Test_23/ex28.py
@@ -0,0 +1,9 @@
+# Insert the correct snippet so that the program produces the expected output.
+# Expected output:
+
+True
+
+list = [False, True, '2', 3, 4, 5]
+b = 0 in list
+print(b)
+
