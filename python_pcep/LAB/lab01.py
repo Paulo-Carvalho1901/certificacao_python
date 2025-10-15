82999830d1b9046a07d763bc5a92e4188d6bffc1
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-15 16:23:01 -0300

    função print

---
+++ python_pcep/LAB/lab01.py
@@ -0,0 +1,11 @@
+# print'Olá Python'
+# print(Paulo)
+# print('Python') print('Ola')
+# print("Ola"
+
+
+print("Olá mundo!")
+print("Paulo")
+
+# função print() é uma função versatil, que trabalha com todos os tipó de dados
+# ele mostra a informação passa no cosole, e é vastamente utilizada pelos programadores
