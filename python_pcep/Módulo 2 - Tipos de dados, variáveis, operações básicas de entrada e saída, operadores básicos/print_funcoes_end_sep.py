e63162f9129da0f727aa621a43310e93a793500a
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-15 21:26:11 -0300

    funções com print

---
+++ python_pcep/Módulo 2 - Tipos de dados, variáveis, operações básicas de entrada e saída, operadores básicos/print_funcoes_end_sep.py
@@ -0,0 +1,17 @@
+print("Meu nome é", "Python.", end=" ")
+print("Monty Python.")
+
+print()
+
+print("Meu nome é", "Python.", end="")
+print("Monty Python.")
+
+print()
+
+print("Meu", "nome", "é", "Monty", "Python.", sep="-")
+
+print()
+
+print("Meu", "nome", "é", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
---
+++ python_pcep/Módulo 2 - Tipos de dados, variáveis, operações básicas de entrada e saída, operadores básicos/print_varios argumentos.py
@@ -0,0 +1,5 @@
+print("A aranha pequenininha" , "subiu" , "a tromba d'água.")
+
+# prevendo a saida do codigo!
+print("Meu nome é", "Python.")
+print("Monty Python.")
