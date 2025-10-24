d12ad3aab310f36cb6f95645d4535a4a5a8df3d0
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-24 14:41:24 -0300

    feat: conclued lab04

---
+++ python_pcep/LAB/lab04.py
@@ -0,0 +1,10 @@
+# Literais Python - strings
+
+"""
+Cenário
+Escreva um código de uma linha usando a função de print(), bem como os caracteres de nova linha e de escape, 
+para corresponder ao resultado esperado gerado em três linhas.
+"""
+
+print('"Eu sou"\n"aprendizado"\n"Python"')
+print("\"Eu sou\"\n\"\"aprendizado\"\"\n\"\"\"Python\"\"\"")
