061a1fb1aa5ce057bb8083ee91be035b7d77ffa4
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-25 17:44:36 -0300

    operadores artimeticos

--- python_pcep/Módulo 2 - Tipos de dados, variáveis, operações básicas de entrada e saída, operadores básicos/python_calculadora.01.py
+++ python_pcep/Módulo 2 - Tipos de dados, variáveis, operações básicas de entrada e saída, operadores básicos/python_calculadora.01.py
@@ -3,4 +3,35 @@
 print(2 + 2)
 print(2 * 3)
 print(4 - 2)
+print()
+# Operadores basicos
 
+"""
++
+-
+*
+/
+//
+%
+**
+"""
+
+print('Exponenciação')
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. ** 3.)
+
+print()
+print('Multiplicação')
+print(2 * 3)
+print(2 * 3.)
+print(2. * 3)
+print(2. * 3.)
+
+print()
+print('Divisão')
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
